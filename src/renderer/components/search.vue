dbfe3c70f0901ba8e4cfb0d64c0c20a7165c6a82
Borja Rojano <borja.rojano@gmail.com>
2018-05-09 22:22:39 +0200

    Tiles look cool with good size title

--- src/renderer/components/search.vue
+++ src/renderer/components/search.vue
@@ -1,5 +1,35 @@
 <template>
-  <p>Hello World</p>
+  <div class="container is-fluid">
+  <div class="columns">
+<!-- Search column -->
+      <div class="column is-2">
+        <p>Search</p>
+      </div>
+<!-- Preview column -->
+      <div class="column is-10">
+        <div class="tile is-ancestor preview-row">
+          <div class="tile is-parent">
+            <div class="tile is-child box">
+              <p class="title is-size-4">Method of identifying chicken variety by DNA strip shape coding</p>
+              <p>The invention discloses a method to appraise chicken species with DNA encoding strip in molecular biology technical domain, which comprises the following steps: utilizing DNA test-sequence technology; checking gene sequence of the chicken group and standard control sample mitochondria cell coloring matter C oxidase I(cytochrome C oxidase I COI); proceeding chicken specie checking according to point of discontinuity of COI sequence, haploid type, haploid type multiple degree (Hd), nucleic acid multiple degree (Pi), genetic distance of double parametric model among species, nucleic acid branch degree among species, specific site of each specie and DNA classified image. This invention can reflect the difference among species compared to the other methods, which result can be scientific proof to appraise genuine-fake of chicken species.</p>
+              <figure class="image image is-2by3">
+                <img src="https://patentimages.storage.googleapis.com/pages/US5455008-1.png">
+              </figure>
+            </div>
+          </div>
+          <div class="tile is-parent">
+            <div class="tile is-child box">
+              <p class="title title is-size-4">Method of identifying chicken variety by DNA strip shape coding</p>
+              <p>The invention discloses a method to appraise chicken species with DNA encoding strip in molecular biology technical domain, which comprises the following steps: utilizing DNA test-sequence technology; checking gene sequence of the chicken group and standard control sample mitochondria cell coloring matter C oxidase I(cytochrome C oxidase I COI); proceeding chicken specie checking according to point of discontinuity of COI sequence, haploid type, haploid type multiple degree (Hd), nucleic acid multiple degree (Pi), genetic distance of double parametric model among species, nucleic acid branch degree among species, specific site of each specie and DNA classified image. This invention can reflect the difference among species compared to the other methods, which result can be scientific proof to appraise genuine-fake of chicken species.</p>
+              <figure class="image image is-2by3">
+                <img src="https://patentimages.storage.googleapis.com/pages/US5455008-1.png">
+              </figure>
+            </div>
+          </div>
+        </div>
+      </div>
+  </div>
+  </div>
 </template>
 
 <script>
@@ -8,4 +38,7 @@ export default {
 </script>
 
 <style scoped>
+  .preview-row{
+    margin-top: 20px;
+  }
 </style>
